289c15a0c6be74cfbb95a2b12238a0cbf87d5150
thekaleidoscope <yadhuksp@gmail.com>
2018-06-05 17:28:18 +0530

    Ropsten contract deployed usinf infura

--- truffle.js
+++ truffle.js
@@ -1,28 +1,44 @@
-module.exports = {
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var mnemonic ="casual tent october favorite swarm eagle genius media note fly creek conduct";
+
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
+
+module.exports = {
   networks: {
-      solc: {
-   optimizer: {
-     enabled: true,
-     runs: 200
-   }
- },
-      ropsten: {
-     host: "127.0.0.1",
-     port: 8545,
-     network_id: 3,
-     gas: 4700000,
-      gasPrice: 21000000000
-   },
-    ganache: {
-      host: "127.0.0.1",
-      port: 7545,
-      network_id: "*" // matching any id
+    development: {
+      host: '127.0.0.1',
+      port: 8545,
+      network_id: '*' // Match any network id
+    },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/qNjs8vdB2KGt3QSe53cG")
+      },
+      network_id: 3,
+      // from:"0x656b1BAeD7C7f8B561dCE054E91239b699b4a0EF",
+      gas: 6612388,
+      gasPrice: 20000000000
   },
-  rpc: {
-host: 'localhost',
-post:8080
-  }
-  }
-};
+
+  ganache:
+  {
+    host: "127.0.0.1",
+    port: 7545,
+    network_id: "*" // matching any id
+    },
+    rpc:
+     {
+        host: 'localhost',
+        post:8080
+    },
+    solc:
+    {
+        optimizer: {
+        enabled: true,
+        runs: 200
+                    }
+    }
+
+}
+}
